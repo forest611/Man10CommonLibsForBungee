d08a8c569a6f3e989a403fbd88132b02ee33ec87
forest611 <forest611328@gmail.com>
2020-05-08 04:41:46 +0900

    Initial commit

---
+++ src/main/java/red/man10/man10commonlibs/Man10CommonLibs.java
@@ -0,0 +1,16 @@
+package red.man10.man10commonlibs;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Man10CommonLibs extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
